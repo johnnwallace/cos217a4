05a87fd93e8bedfa2a40b2f1233cb4e9f23a6bd4
John Wallace <jw0535@princeton.edu>
2023-04-05 11:21:27 -0400

    removed debug printing

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -87,7 +87,7 @@ static boolean CheckerDT_treeCheck(Node_T oNNode) {
             /* DO WE NEED TO CHECK iStatus???? */
 
             if(Node_compare(oNChild, oNChildB) == 0) {
-                fprintf(stderr, "More than one identical nodes at %s: (%ld) (%ld)\n",
+                fprintf(stderr, "More than one identical nodes at %s\n",
                     Path_getPathname(Node_getPath(oNNode)), ulIndex, ulIndexB);
                 return FALSE;
             }
