b5949cca5fd44fa0ee4440489a64dd0f251f85bf
John Wallace <jw0535@princeton.edu>
2023-04-09 21:42:32 -0400

    client output

--- 3FT/node_client.c
+++ 3FT/node_client.c
@@ -52,7 +52,7 @@ int main(void) {
     printf("printing return val of hasChild on nodeA, shoudl be TRUE:\n");
     printf("%d\n", (int)Node_hasChild(nodeA, pathB, &ulIndex));
 
-    printf("freeing %d nodes", Node_free(nodeA));
+    printf("freeing %ld nodes", Node_free(nodeA));
 
     return 0;
 }
