5bc50124884956826b783a6de6d8b20bb30b1d3a
John Wallace <jw0535@princeton.edu>
2023-04-13 18:06:31 -0400

    addressing splint warnings

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -54,7 +54,7 @@ static int FT_traversePath(Path_T oPPath, Node_T *poNFurthest) {
     Node_T oNChild = NULL;
     size_t ulDepth;
     size_t i;
-    size_t ulChildID;
+    size_t ulChildID = 0;
 
     assert(oPPath != NULL);
     assert(poNFurthest != NULL);
